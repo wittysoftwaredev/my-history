ed59ea5386251a30f6c204f2e5a00b55853049bf
wittysoftwaredev <maxcore1120@gmail.com>
2025-07-23 16:30:22 +0700

    hotfix: change main logic of logger.js

---
+++ src/logger.js
@@ -0,0 +1,43 @@
+const ProgressBar = require('progress');
+
+const verboseMode = process.env.DEBUG_DOC_REDUCER === 'true';
+let bar;
+
+const progress = {
+  init: (total, action) => {
+    if (bar) {
+      bar.terminate();
+      bar = null;
+    }
+
+    const formattedAction = `  ${action}: |:bar| :current of :total`;
+    bar = new ProgressBar(formattedAction, {
+      total,
+      complete: '=',
+      incomplete: ' ',
+      width: 30,
+      renderThrottle: 0
+    });
+  },
+  updateCurrent: count => {
+    bar.tick(count);
+  },
+  addToTotal: total => {
+    bar.total += total;
+  },
+  end: () => {
+    bar.terminate();
+  }
+};
+
+const log = (...args) => {
+  if (verboseMode) {
+    // eslint-disable-next-line no-console
+    console.log(...args);
+  }
+};
+
+module.exports = {
+  progress,
+  log
+};
