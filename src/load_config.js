cea3b2d25a490120f199c4361f5e6166668cbe11
wittysoftwaredev <maxcore1120@gmail.com>
2025-07-23 12:31:32 +0700

    Add main logic of load_config.js

---
+++ src/load_config.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const logger = require('./logger.js');
+
+const directoryState = Object.freeze({
+  org: '',
+  repo: '',
+  dir: '',
+  isReadme: false
+});
+
+const loadConfig = () => {
+  const config = { all: {}, directories: [] };
+
+  config.all = JSON.parse(fs.readFileSync(`${process.cwd()}/doc-reducer.json`, 'utf8'));
+
+  config.all.sources.forEach(src => {
+    const orgName = src.org;
+    src.repos.forEach(repo => {
+      const repoName = repo.name;
+      repo.directories.forEach(dir => {
+        const dirState = { ...directoryState, org: orgName, repo: repoName, dir };
+        config.directories.push(dirState);
+      });
+
+      if (repo.readmeName) {
+        config.directories.push({
+          ...directoryState,
+          org: orgName,
+          repo: repoName,
+          dir: repo.readmeName,
+          isReadme: true
+        });
+      }
+    });
+  });
+
+  logger.log(config);
+  return config;
+};
+
+module.exports = loadConfig;
